8961f217a69b5206b9a61318be706e7fbf5e264f
ronit <ronitkhalate243@gmail.com>
2023-07-07 13:12:52 +0530

    bottom navigation working
    bottom navigation is working now we can change screen with bottom
    navigations

--- app/src/main/java/com/example/noms/MainActivity.kt
+++ app/src/main/java/com/example/noms/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.noms
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.noms.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+////    //? Navigation Setup
+        val bottomNavigation=binding.bottomNav
+        navController=Navigation.findNavController(this,R.id.host_fragment)
+        NavigationUI.setupWithNavController(bottomNavigation,navController)
     }
 }
